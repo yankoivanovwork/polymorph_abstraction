3995be12d3134f49081323566909f532e4bc0bae
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-05 09:48:57 +0300

    Update BasicOperation.cs

--- abstraction_test/BasicOperation.cs
+++ abstraction_test/BasicOperation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace abstraction_test
 {
-    class BasicOperation : Calculator
+    public class BasicOperation : Calculator
     {
         private double result;
         private int choice;
